43311bf0a0de3bbe753ea10ce4631f2f2b480b94
Sameer092 <sameershazad501@gmail.com>
2023-07-25 22:29:16 +0500

    Complete states and props work

---
+++ src/Components/Newitems.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Newitems extends Component {
+    render() {
+        let { title, description, imageUrl, newsUrl } = this.props;
+        return (
+            <div className="my-3">
+                <div className="card " style={{ width: "18rem" }}>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href={newsUrl} target='_blank' rel="noreferrer"  className="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
