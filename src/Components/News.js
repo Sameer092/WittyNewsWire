ea8b9c42f115d6ef950d363499cdec29e101a49a
Sameer092 <sameershazad501@gmail.com>
2023-07-27 19:57:28 +0500

    Loading images spinner

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Newitems from './Newitems'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
-
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
     static defaultProps = {
@@ -26,8 +26,9 @@ export default class News extends Component {
         super(props);
         this.state = {
             articles: [],
-            loading: false,
-            page: 1
+            loading: true,
+            page: 1,
+            totalResults: 0
         }
         document.title = `WittyNewsWire - ${this.capatilize(this.props.category)}`
     }
@@ -40,7 +41,7 @@ export default class News extends Component {
         let parsedData = await data.json()
         this.setState({
             articles: parsedData.articles,
-            totalresults: parsedData.totalResults,
+            totalResults: parsedData.totalResults,
             loading: false
         })
     }
@@ -49,37 +50,41 @@ export default class News extends Component {
         this.updateNews();
     }
 
-    nextfunc = async () => {
+    fetchMoreData = async () => {
         this.setState({
             page: this.state.page + 1
         })
-        this.updateNews()
-    }
-
-    previousfunc = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f358ed0ff318429e8280823192ba285c&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        let data = await fetch(url)
+        let parsedData = await data.json()
         this.setState({
-            page: this.state.page - 1
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults
         })
-        this.updateNews()
-    }
+    };
 
     render() {
         return (
-            <div className='container my-3'>
+            < >
                 <h1 className="text-center" style={{ margin: '30px' }}>{`WittyNewsWire - Top ${this.capatilize(this.props.category)} Headlines`}</h1>
                 {this.state.loading && <Spinner />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <Newitems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                >
+                    <div className="container">
+                        <div className="row">
+                            {this.state.articles.map((element) => {
+                                return <div className="col-md-4" key={element.url}>
+                                    <Newitems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                </div>
+                            })}
                         </div>
-                    })}
-                </div>
-                <div className="container d-flex justify-content-between">
-                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.previousfunc}>&larr; Previous</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalresults / 20)} type="button" className="btn btn-dark" onClick={this.nextfunc}>Next &rarr;</button>
-                </div>
-            </div>
+                    </div>
+                </InfiniteScroll>
+            </>
         )
     }
 }
